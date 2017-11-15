d99936991f31f45bebd852ad523614b01bdde2d4
danesfeder <dan.nesfeder@mapbox.com>
2017-11-15 18:17:05 -0500

    Add custom themes via XML for light / dark mode

--- libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/NavigationView.java
+++ libandroid-navigation-ui/src/main/java/com/mapbox/services/android/navigation/ui/v5/NavigationView.java
@@ -91,7 +91,7 @@ public class NavigationView extends CoordinatorLayout implements OnMapReadyCallb
   public NavigationView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
-    ThemeSwitcher.setTheme(getContext());
+    ThemeSwitcher.setTheme(getContext(), attrs);
     init();
   }
 
